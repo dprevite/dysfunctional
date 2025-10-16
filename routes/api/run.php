27b0a23a3c3439d3285e7c450dc463a308e92bca
Dan Previte <dprevite@gmail.com>
2025-10-16 23:50:17 +0000

    /run routes

--- routes/api/run.php
+++ routes/api/run.php
@@ -6,8 +6,8 @@ use App\Http\Controllers\Api\RunController;
 
 Route::prefix('run')
     ->group(function () {
-        Route::post(
-            uri: '/test',
+        Route::any(
+            uri: '{any?}',
             action: [RunController::class, 'handle']
-        );
+        )->where('any', '.*');
     });
