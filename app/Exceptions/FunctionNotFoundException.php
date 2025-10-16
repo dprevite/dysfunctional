51ab6c5908e3adc8c2befd3087091d030263519e
Dan Previte <dprevite@gmail.com>
2025-10-16 23:48:02 +0000

    Add exception

---
+++ app/Exceptions/FunctionNotFoundException.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Exceptions;
+
+use Exception;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class FunctionNotFoundException extends Exception
+{
+    /**
+     * Render the exception as an HTTP response.
+     */
+    public function render(Request $request): JsonResponse
+    {
+        return response()->json([
+            'error' => 'Function not found',
+            'message' => $this->getMessage(),
+        ], 404);
+    }
+}
